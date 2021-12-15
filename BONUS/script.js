1f1a965bacd003525f80e2b23092c1a768ea0dca
AngeloDalo <angelo251158@gmail.com>
2021-12-15 14:37:22 +0100

    bottone

--- BONUS/script.js
+++ BONUS/script.js
@@ -12,5 +12,12 @@
      message: 'Mars',
      image: 'https://www.castfvg.it/sistsola/marte/nasa/marte_017.jpg',
      text: '',
+     object: 'https://uploads-ssl.webflow.com/5f81783dfacc3a7fa55483ad/5f9b4763326bae0e330efb72_Artboard%201HLV.png',
+     number: 0
+    },
+    methods: {
+        addOneObject: function() {
+            this.number += 1;
+        }
     }
 })
